a7da6dbbbc74723f5c4afc1f4a9a12b42e527542
okok111 <1for.rails@gmail.com>
2024-08-10 02:56:08 +0900

    3-4

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'hello/index' => 'hello#index'
+  get 'hello/link' => 'hello#link'
+  get 'tweets' => 'tweets#index'
   root 'hello#index'
 
   # Defines the root path route ("/")
