6b52ad336912b0e52073a9bc6147990d4edfa8cb
okok111 <1for.rails@gmail.com>
2024-08-11 09:45:23 +0900

    3-13

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'tweets' => 'tweets#index'
   get 'tweets/new' => 'tweets#new'
   post 'tweets' => 'tweets#create'
+  get 'tweets/:id' => 'tweets#show',as: 'tweet'
   root 'hello#index'
 
   # Defines the root path route ("/")
